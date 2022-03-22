d6c8ab80f8aff7e013474eb69c80ee946e7b7a62
Ponhik <Ponhik@DESKTOP-3FM658V>
2022-03-22 23:03:12 +0700

    Добавьте файлы проекта.

---
+++ Lesson2_Number of points/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2_Number_of_points
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double ProgrammingMark = 98.5;// баллы по програмированию 
+            double MathsMark = 76.9;//баллы по математике
+            double PhysicsMark = 86.4;// баллы пр физике 
+            
+            double SumMark = ProgrammingMark + MathsMark + PhysicsMark;// сумма баллов 
+            double MediumMark = SumMark / 3;// средний балл
+
+            string FormatMark = MediumMark.ToString("#.#");// формат ввывода среднего балла
+
+            Console.WriteLine("Сумма баллов: " + SumMark);
+
+            Console.ReadKey();
+
+            Console.WriteLine("\nСредний балл: " + FormatMark);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Lessson2_Variables/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessson2_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string FullName = "Петров Сергей Леонидович";//Ф.И.О
+            int Years = 19;//возраст
+            string Email = "petrov_sl@gmail.com";//email
+            double ProgrammingMark = 98.5;// баллы по програмированию 
+            double MathsMark = 76.9;//баллы по математике
+            double PhysicsMark = 86.4;// баллы пр физике 
+
+            Console.WriteLine($"Ф.И.О: {FullName} \nВозраст: {Years} \nEmail: {Email} \nБаллы по програмированию: {ProgrammingMark} \nБаллы по матетике: {MathsMark} \nБаллы по физике: {PhysicsMark}");
+            
+            Console.ReadKey();
+
+        }
+    }
+}
